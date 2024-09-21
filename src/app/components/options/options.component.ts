faf4f9bcc2f2f56497a5da98e228956bfb176f7d
Ayush Yadav <aayush0029@gmail.com>
2024-09-21 16:29:37 +0530

    Added Options List

---
+++ src/app/components/options/options.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-options',
+  standalone: true,
+  imports: [MatIcon],
+  templateUrl: './options.component.html',
+  styleUrl: './options.component.scss'
+})
+export class OptionsComponent {
+
+}
--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
+import { OptionsComponent } from "../options/options.component";
 
 @Component({
   selector: 'app-question',
   standalone: true,
-  imports: [MatIconModule,MatMenuModule,MatButtonModule],
+  imports: [MatIconModule, MatMenuModule, MatButtonModule, OptionsComponent],
   templateUrl: './question.component.html',
   styleUrl: './question.component.scss'
 })
